19c8a5ee7adb8919971108c98f128515fa1d17f6
Aleksandr Alekseev <alekseev.a@atbgroup.ru>
2024-12-17 11:44:29 +0300

    init repo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,16 @@
-import {Controller, Request, Post, UseGuards} from '@nestjs/common';
+import {Controller, Request, Post, Body, UseGuards} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import {AuthService} from "./auth/auth.service";
+import {AuthService, User} from "./auth/auth.service";
 
 @Controller()
 export class AppController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() user: User): Promise<{ access_token: string }> {
+    const token = await this.authService.login(user);
+    return { access_token: token };
+  }
 
   @UseGuards(AuthGuard('jwt'))
   @Post('profile')
